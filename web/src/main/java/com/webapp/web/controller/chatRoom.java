6cc0e61010dc9a31b2cf20ddc77654492fcc0a0e
张天一 <1539626240@qq.com>
2022-08-05 18:02:19 +0800

    log4j不兼容

--- web/src/main/java/com/webapp/web/controller/chatRoom.java
+++ web/src/main/java/com/webapp/web/controller/chatRoom.java
@@ -1,14 +1,14 @@
 package com.webapp.web.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "/base")
 public class chatRoom {
-    private static final Logger logger = LoggerFactory.getLogger(chatRoom.class);
+//    private static final Logger logger = LoggerFactory.getLogger(chatRoom.class);
     public static final String INDEX_JSP = "";
     public static final String LOGIN_JSP = "login";
     public static final String MAIN_JSP = "";
@@ -21,7 +21,7 @@ public class chatRoom {
     @RequestMapping(value = "/login")
     public String login() {
         System.out.println("dwadadwa");
-        logger.info("dwadadwa");
+//        logger.info("dwadadwa");
         return LOGIN_JSP;
     }
 
